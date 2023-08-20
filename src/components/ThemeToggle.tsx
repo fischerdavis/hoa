2c666b7f7eddeaa8f6888caf9c3d772223388a26
Fischer Davis <fischerdavis94@gmail.com>
2023-08-19 21:35:50 -0500

    Started adding support for shadcn components.

---
+++ src/components/ThemeToggle.tsx
@@ -0,0 +1,35 @@
+import * as React from "react";
+import { useTheme } from "next-themes";
+import { Icons } from "@/components/ui/Icons";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/Tooltip";
+import { Button } from '@/components/ui/Button';
+
+export function ThemeToggle() {
+  const { setTheme, theme } = useTheme();
+
+  return (
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Button
+            variant="ghost"
+            size="sm"
+            onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+          >
+            <Icons.sun className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+            <Icons.moon className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+            <span className="sr-only">Toggle theme</span>
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>Toggle {theme === "light" ? "Dark" : "Light"} Mode</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+}
